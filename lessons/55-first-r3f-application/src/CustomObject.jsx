ea72ee6969f9a33d53cb27a7329b942783d0abc2
Meor Hasyim <meorhashim@gmail.com>
2024-09-06 22:29:03 +0800

    update custom geometry

--- lessons/55-first-r3f-application/src/CustomObject.jsx
+++ lessons/55-first-r3f-application/src/CustomObject.jsx
@@ -1,21 +1,34 @@
 import { DoubleSide } from "three"
+import { useEffect, useRef, useMemo } from "react"
 
 export default function CustomObject()
 {
+    const geometryRef = useRef()
     const verticesCount = 10 * 3
-    const positions = new Float32Array(verticesCount * 3)
+    
+    const positions = useMemo(() =>
+    {
+        const positions = new Float32Array(verticesCount * 3)
 
-    for (let i = 0; i < verticesCount; i++)
-        positions[i] = (Math.random() - 0.5) * 3
+        for (let i = 0; i < verticesCount * 3; i++)
+            positions[i] = (Math.random() - 0.5) * 3
+
+        return positions
+    })
+
+    useEffect(() =>
+    {
+        geometryRef.current.computeVertexNormals()
+    }, [ positions ])
 
     return <mesh>
-            <bufferGeometry>
+            <bufferGeometry ref={ geometryRef }>
                 <bufferAttribute
                     attach="attributes-position"
                     count={verticesCount}
                     itemSize={ 3 }
                     array={ positions }/>
-                <meshBasicMaterial color="red" side={ DoubleSide } />
-            </bufferGeometry>  
+            </bufferGeometry> 
+            <meshStandardMaterial color="red" side={ DoubleSide } /> 
         </mesh>
 }
