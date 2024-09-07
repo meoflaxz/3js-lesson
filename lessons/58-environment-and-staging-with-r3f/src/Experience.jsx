34e7fc76f91761a2f760ff890cd728a036967f54
Meor Hasyim <meorhashim@gmail.com>
2024-09-08 00:20:06 +0800

    add shadows

--- lessons/58-environment-and-staging-with-r3f/src/Experience.jsx
+++ lessons/58-environment-and-staging-with-r3f/src/Experience.jsx
@@ -1,10 +1,13 @@
 import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { BakeShadows, useHelper, OrbitControls } from '@react-three/drei'
 import { useRef } from 'react'
 import { Perf } from 'r3f-perf'
+import * as THREE from 'three'
 
 export default function Experience()
 {
+    const directionalLight = useRef()
+    useHelper(directionalLight, THREE.DirectionalLightHelper, 1)
     const cube = useRef()
     
     useFrame((state, delta) =>
@@ -13,25 +16,28 @@ export default function Experience()
     })
 
     return <>
+        <BakeShadows/>
+        
+        <color args={['#ivory']} attach={'background'}/>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
+        <directionalLight ref={ directionalLight } castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
 
-        <mesh position-x={ - 2 }>
+        <mesh castShadow position-x={ - 2 }>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+        <mesh castShadow ref={ cube } position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
--- lessons/58-environment-and-staging-with-r3f/src/index.jsx
+++ lessons/58-environment-and-staging-with-r3f/src/index.jsx
@@ -2,11 +2,13 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
+import * as THREE from 'three'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <Canvas
+        shadows
         camera={ {
             fov: 45,
             near: 0.1,
