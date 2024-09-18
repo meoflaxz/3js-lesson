bdc4141243559b02e00d66d168fa06ab908dda12
aiem2x <meor.hasyim@2x.marketing>
2024-09-18 20:29:45 +0800

    completed 62-mouse-events lesson

--- lessons/62-mouse-events-with-r3f/src/Experience.jsx
+++ lessons/62-mouse-events-with-r3f/src/Experience.jsx
@@ -1,10 +1,17 @@
 import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { meshBounds, useGLTF,OrbitControls } from '@react-three/drei'
 import { useRef } from 'react'
 
 export default function Experience()
 {
+    const hamburger = useGLTF('./hamburger.glb') 
     const cube = useRef()
+
+    const eventHandler = (event) =>
+    {
+        console.log(event)
+        cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`)
+    }
     
     useFrame((state, delta) =>
     {
@@ -18,12 +25,22 @@ export default function Experience()
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
 
-        <mesh position-x={ - 2 }>
+        <mesh
+            position-x={ - 2 }
+            onClick={ (event) => event.stopPropagation() }
+            onPointerEnter={ (event) => event.stopPropagation() }>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+        <mesh
+            ref={ cube }
+            raycast={ meshBounds }
+            position-x={ 2 }
+            scale={ 1.5 }
+            onClick= {eventHandler}
+            onPointerEnter={ () => { document.body.style.cursor = 'pointer' } }
+            onPointerLeave={ () => { document.body.style.cursor = 'default' } }>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
@@ -32,6 +49,15 @@ export default function Experience()
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
+
+        <primitive
+            object={ hamburger.scene }
+            scale={0.25}
+            position-y={0.5}
+            onClick={(event) =>
+            {
+                event.stopPropagation()
+            }}/>
 
     </>
 }
--- lessons/62-mouse-events-with-r3f/src/index.jsx
+++ lessons/62-mouse-events-with-r3f/src/index.jsx
@@ -2,6 +2,7 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
+import { Bvh } from '@react-three/drei'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
@@ -13,7 +14,10 @@ root.render(
             far: 200,
             position: [ - 4, 3, 6 ]
         } }
+        onPointerMissed={ () => { console.log('You missed!') } }
     >
-        <Experience />
+        <Bvh>
+            <Experience />
+        </Bvh>
     </Canvas>
 )
