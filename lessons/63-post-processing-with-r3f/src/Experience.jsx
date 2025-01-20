bef16e9b20554ad0e7b8ad3e3d5d7d8c8a339317
Meor Hasyim <40785172+meoflaxz@users.noreply.github.com>
2025-01-21 00:28:28 +0800

    Updated

---
+++ lessons/63-post-processing-with-r3f/src/Experience.jsx
@@ -0,0 +1,39 @@
+import { OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+import { ToneMapping,Vignette, EffectComposer } from '@react-three/postprocessing'
+import { BlendFunction, ToneMappingMode } from 'postprocessing'
+export default function Experience()
+{
+    return <>
+        <EffectComposer>
+            <ToneMapping mode={ ToneMappingMode.ACES_FILMIC } />
+            <Vignette
+                offset={ 0.3 }
+                darkness={ 0.5 }
+                blendFunction={ BlendFunction.NORMAL }/>
+        </EffectComposer>
+
+        <Perf position="top-left" />
+
+        <OrbitControls makeDefault />
+
+        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
+        <ambientLight intensity={ 1.5 } />
+
+        <mesh castShadow position-x={ - 2 }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
+
+        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+
+        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+    </>
+}
