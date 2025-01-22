8d6bef7d7ce6fb012c77550b9bd5621859d53c79
Meor Hasyim <40785172+meoflaxz@users.noreply.github.com>
2025-01-22 23:49:04 +0800

    Update

--- lessons/64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ lessons/64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -18,10 +18,10 @@ export default function Experience()
         <PresentationControls
             global
             rotation={ [ 0.13, 0.1, 0]}
-            // polar={ [ -0.4, 0.2 ]}
-            // azimuth={ [ -1, 0.75 ]}
-            // config={ {mass: 2, tension: 400 }}
-            // snap={ { mass: 4, tension: 400 } }
+            polar={ [ -0.4, 0.2 ]}
+            azimuth={ [ -1, 0.75 ]}
+            config={ {mass: 2, tension: 400 }}
+            snap={ { mass: 4, tension: 400 } }
             >
             <Float rotationIntensity={ 0.4 }>
                 <rectAreaLight
